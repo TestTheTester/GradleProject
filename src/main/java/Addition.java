37f1191ac9f08defd2c119a391fe0177633c1859
rahul.sinha <sinharahul51@yahoo.in>
2020-10-22 13:13:41 +0530

    initial checkin

---
+++ src/main/java/Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int s = 10+5;
+        System.out.println("Adding 10 + 5...");
+        System.out.println("Result="+ s);
+    }
+}
