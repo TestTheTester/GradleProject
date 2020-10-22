0d82273607c18ae85547ae872b1e6303cb72a855
rahul.sinha <>
2020-10-22 20:43:39 +0530

    added tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -12,4 +12,13 @@ public class CalculatorTest {
         calc.sum();
         assertEquals(15, calc.getResult());
     }
+
+    @Test
+    public void testSubtraction() {
+        Calculator calc = new Calculator();
+        calc.setInputA(10);
+        calc.setInputB(5);
+        calc.minus();
+        assertEquals("Calculation failed:", 5, calc.getResult());
+    }
 }
